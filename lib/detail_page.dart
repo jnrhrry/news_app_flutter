6949a9d01d449dc17aa489d3531810bbf0b2e692
Harry Januar <januarjanuarharry@gmail.com>
2020-10-20 13:21:52 +0700

    first commit

---
+++ lib/detail_page.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+import 'article.dart';
+
+class ArticleDetailpage extends StatelessWidget {
+  static const routeName = '/article_detail';
+
+  final Article article;
+  const ArticleDetailpage({@required this.article});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(article.title),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Image.network(article.urlToImage),
+            Padding(
+              padding: EdgeInsets.all(10),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(article.description),
+                  Divider(
+                    color: Colors.grey,
+                  ),
+                  Text(
+                    article.title,
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 24),
+                  ),
+                  Divider(
+                    color: Colors.grey,
+                  ),
+                  Text('Date: ${article.publishedAt}'),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text('Author: {article.author}'),
+                  Divider(
+                    color: Colors.grey,
+                  ),
+                  Text(
+                    article.content,
+                    style: TextStyle(fontSize: 16),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  RaisedButton(child: Text('Read More'), onPressed: () {})
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ArticleWebView extends StatelessWidget {
+  static const routeName = '/article_web';
+  final String url;
+  const ArticleWebView({@required this.url});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('News Today'),
+      ),
+      body: WebView(
+        initialUrl: url,
+      ),
+    );
+  }
+}
